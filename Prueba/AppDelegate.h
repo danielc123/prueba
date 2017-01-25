09c0dbeba165289f03ddf9edceacff0de95b0e71
cice <a>
2017-01-25 19:39:51 +0100

    Initial Commit

---
+++ Prueba/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prueba
+//
+//  Created by cice on 25/1/17.
+//  Copyright © 2017 cice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Prueba/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Prueba
+//
+//  Created by cice on 25/1/17.
+//  Copyright © 2017 cice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
